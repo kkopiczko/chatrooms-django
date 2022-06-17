8b0ba8b5429d476781a03eb43434c0388b3b0b39
Kamila <kamkopiczko@gmail.com>
2022-06-17 18:43:14 +0200

    retrieving rooms from db

--- studychatrooms/baseapp/views.py
+++ studychatrooms/baseapp/views.py
@@ -2,29 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 
-rooms = [
-    {
-        'id': 1,
-        'name': 'Let\'s learn python'
-    },
-    {
-        'id': 2,
-        'name': 'Javascript'
-    },
-    {
-        'id': 3,
-        'name': 'Spaghetti fans'
-    }
-]
+from .models import Room
+
+# rooms = [
+#     {
+#         'id': 1,
+#         'name': 'Let\'s learn python'
+#     },
+#     {
+#         'id': 2,
+#         'name': 'Javascript'
+#     },
+#     {
+#         'id': 3,
+#         'name': 'Spaghetti fans'
+#     }
+# ]
 
 def home(req):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(req, 'baseapp/home.html', context)
 
 def room(req, pk):
-    for r in rooms:
-        if r['id'] == int(pk):
-            room = r
+    # for r in rooms:
+    #     if r['id'] == int(pk):
+    #         room = r
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(req, 'baseapp/room.html', context)
 
